682466bb8103020d911554c1ace46147236ae4b6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-23 10:37:18 -0400

    use before spec to clear rooms and users

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -9,7 +9,26 @@ const database = require('../../app/database')
 // we should have at least two mongoose database models
 // { user: ..., room: ... }
 
+async function clearRooms() {
+  console.log('clear rooms')
+  await database.models.room.deleteMany({})
+  return null
+}
+
+async function clearUsers() {
+  console.log('clear users')
+  await database.models.user.deleteMany({})
+  return null
+}
+
 module.exports = (on, config) => {
+  // https://on.cypress.io/before-spec-api
+  on('before:spec', async (spec) => {
+    console.log('before:spec', spec.name)
+    await clearRooms()
+    await clearUsers()
+  })
+
   on('task', {
     async getUsers() {
       console.log('getUsers')
@@ -24,16 +43,7 @@ module.exports = (on, config) => {
       return users
     },
 
-    async clearUsers() {
-      console.log('clear users')
-      await database.models.user.deleteMany({})
-      return null
-    },
-
-    async clearRooms() {
-      console.log('clear rooms')
-      await database.models.room.deleteMany({})
-      return null
-    },
+    clearUsers,
+    clearRooms,
   })
 }
