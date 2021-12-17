8666c3b2ae981b4c4142419d0e06d614a1d956ac
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-12-17 12:59:26 -0500

    clean up the spec

--- cypress/integration/clear-rooms-if-needed.js
+++ cypress/integration/clear-rooms-if-needed.js
@@ -4,10 +4,45 @@ import { registerViaApi } from './utils'
 
 describe('clear rooms if needed', () => {
   beforeEach(() => {
-    // slow down each command to simulate an expensive setup
-    cy.task('clearRooms').wait(1000, { log: false })
-    cy.task('makeRoom', 'attic').wait(1000, { log: false })
-    cy.task('makeRoom', 'kitchen').wait(1000, { log: false })
+    function checkRooms() {
+      return cy.task('getRooms').then((rooms) => {
+        if (
+          rooms.length === 2 &&
+          rooms[0].title === 'attic' &&
+          rooms[1].title === 'kitchen'
+        ) {
+          return { attic: rooms[0]._id, kitchen: rooms[1]._id }
+        }
+        // DANGER: if you return undefined from cy.then
+        // it will yield the original subject
+        return null
+      })
+    }
+
+    cy.dataSession({
+      name: 'two rooms',
+      init: checkRooms,
+      preSetup() {
+        cy.task('clearRooms').wait(1000, { log: false })
+      },
+      setup() {
+        // slow down each command to simulate an expensive setup
+        cy.task('makeRoom', 'attic')
+          .wait(1000, { log: false })
+          .then((attic) => {
+            cy.task('makeRoom', 'kitchen')
+              .wait(1000, { log: false })
+              .then((kitchen) => {
+                return { attic, kitchen }
+              })
+          })
+      },
+      validate(rooms) {
+        return checkRooms().then((loadedRooms) => {
+          return Cypress._.isEqual(rooms, loadedRooms)
+        })
+      },
+    })
   })
 
   it('logs in and sees two rooms', () => {
