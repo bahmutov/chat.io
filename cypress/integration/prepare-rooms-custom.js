8de0a57d50fe7204b7ff1549e8388debaee8b7d1
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-12-17 13:08:03 -0500

    update a test

--- cypress/integration/prepare-rooms-custom.js
+++ cypress/integration/prepare-rooms-custom.js
@@ -3,6 +3,7 @@
 import { registerViaApi } from './utils'
 
 describe('prepare rooms', () => {
+  let atticId
   beforeEach(() => {
     cy.task('getRooms').then((rooms) => {
       // check if the rooms are already prepared
@@ -14,8 +15,14 @@ describe('prepare rooms', () => {
         // otherwise, prepare the rooms
         // slow down each command to simulate an expensive setup
         cy.task('clearRooms').wait(1000, { log: false })
-        cy.task('makeRoom', 'attic').wait(1000, { log: false })
+        cy.task('makeRoom', 'attic')
+          .wait(1000, { log: false })
+          .then((id) => {
+            atticId = id
+          })
         cy.task('makeRoom', 'kitchen').wait(1000, { log: false })
+      } else {
+        atticId = rooms[0]._id
       }
     })
   })
@@ -24,5 +31,7 @@ describe('prepare rooms', () => {
     registerViaApi()
     cy.visit('/rooms')
     cy.get('[data-cy=room]').should('have.length', 2)
+    cy.contains('[data-cy=room]', 'attic').click()
+    cy.location('pathname').should('eq', '/chat/' + atticId)
   })
 })
